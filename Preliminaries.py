34b1f279dbcfc0e768062e959b09131e2c5149ee
Thomas Rochais <thb@sas.upenn.edu>
2021-04-18 18:53:56 -0400

    Proposal draft done

--- Preliminaries.py
+++ Preliminaries.py
@@ -1,5 +1,6 @@
 # Preliminary analysis for the project proposal
 import pandas as pd
+import matplotlib.pyplot as plt
 # Read in the data (which can be downloaded from):
 # https://data.oecd.org/pisa/reading-performance-pisa.htm
 # https://data.oecd.org/emp/employment-rate.htm
@@ -8,6 +9,7 @@ import pandas as pd
 
 Reading = pd.read_csv("../ReadingPerformancePisa.csv")
 EmploymentRate = pd.read_csv("../EmploymentRate.csv")
+SelfEmploymentRate = pd.read_csv("../SelfEmploymentRate.csv")
 EmploymentActivity = pd.read_csv("../EmploymentRateByActivity.csv")
 EmploymentEducation = pd.read_csv("../EmploymentRateByEducationLevel.csv")
 
@@ -17,20 +19,42 @@ DATA = pd.concat([Reading, EmploymentRate, EmploymentActivity, EmploymentEducati
 # Focus on a single year, say 2018:
 Reading = Reading[Reading["TIME"] == 2018]
 EmploymentRate = EmploymentRate[EmploymentRate["TIME"] == "2018"]
+SelfEmploymentRate = SelfEmploymentRate[SelfEmploymentRate["TIME"] == 2018]
 EmploymentActivity = EmploymentActivity[EmploymentActivity["TIME"] == "2018"]
 EmploymentEducation = EmploymentEducation[EmploymentEducation["TIME"] == 2018]
 
 # Just look at the total employment rates regardless of gender:
 # And look at the percentage fo the work force instead of the whole population
 EmploymentRate = EmploymentRate[(EmploymentRate["SUBJECT"] == "TOT") & (EmploymentRate["MEASURE"] == "PC_WKGPOP")]
+SelfEmploymentRate = SelfEmploymentRate[SelfEmploymentRate["SUBJECT"] == "TOT"]
 
+employment_rate = []
+reading_performance = []
+for country in EmploymentRate["LOCATION"].unique():
+    if country in Reading["LOCATION"].unique():
+        employment_rate.append(EmploymentRate[EmploymentRate["LOCATION"] == country]["Value"].values)
+        reading_performance.append(Reading[Reading["LOCATION"] == country]["Value"].values)
+plt.plot(reading_performance, employment_rate, 'o', color = 'k')
+plt.title('Employment rate vs. Reading performace across multiple countries')
+plt.xlabel('PISA reading performace')
+plt.ylabel('Employment rate (\% working pop.)')
+plt.show()
 
-# Focus on the employment rate and reading performance
-Data1 = DATA[((DATA["INDICATOR"] == "EMP") | (DATA["INDICATOR"] == "PISAREAD")) 
-& ((DATA["TIME"] == 2018) | (DATA["TIME"] == "2018")) & (DATA["MEASURE"] == "PC_WKGPOP")]
-print(Data1.head())
+self_employment_rate = []
+reading_performance = []
+for country in SelfEmploymentRate["LOCATION"].unique():
+    if country in Reading["LOCATION"].unique():
+        self_employment_rate.append(SelfEmploymentRate[SelfEmploymentRate["LOCATION"] == country]["Value"].values)
+        reading_performance.append(Reading[Reading["LOCATION"] == country]["Value"].values)
+plt.plot(reading_performance, self_employment_rate, 'o', color = 'k')
+plt.title('Self employment rate vs. Reading performace across multiple countries')
+plt.xlabel('PISA reading performace')
+plt.ylabel('Self employment rate (\% of employment)')
+plt.show()
+
 print(Reading.head())
 print(EmploymentRate.head())
-print(EmploymentActivity.head())
-print(EmploymentEducation.head())
+print(SelfEmploymentRate.head())
+# print(EmploymentActivity.head())
+# print(EmploymentEducation.head())
 
